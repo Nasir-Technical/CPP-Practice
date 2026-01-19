9d1df38d5cdd06bd8f8a2ba9f2fe67c585cb5078
Nasir-Technical <nasir.18.2006@gmail.com>
2026-01-19 20:30:18 +0500

    pratice Logical Operators

--- pratice4.cpp
+++ pratice4.cpp
@@ -34,5 +34,13 @@ int main()
     cout<<"The value of a <= b is "<<(a<=b)<<endl;
     cout<<"The value of a < b is "<<(a<b)<<endl;
     cout<<"The value of a > b is "<<(a>b)<<endl;
+    cout<<endl;
+
+    // Logical Operator 
+    cout<<"Following are the logical operators in C++"<<endl;
+    cout<<"The value of this logical and operator ((a==b) && (a<b)) is: "<<((a==b) && (a<b))<<endl;
+    cout<<"The value of this logical or operator ((a==b) || (a<b)) is: "<<((a==b) || (a<b))<<endl;
+    cout<<"The value of this logical not operator (!(a==b)) is: "<<(!(a==b))<<endl;
+
     return 0;
 }
